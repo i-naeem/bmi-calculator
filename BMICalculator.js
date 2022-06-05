6608384409583a9990cf95286af6b3011ab60d26
i-naeem <inaeem@protonmail.com>
2022-06-05 19:18:58 +0500

    :construction: add javascript

---
+++ BMICalculator.js
@@ -0,0 +1,9 @@
+const findBMI = (height, age) => {
+  return 18.2;
+};
+
+const formatOutput = (bmi) => {
+  return `<p class="mb-0"> Your BMI is <strong> ${bmi} </strong> </p>`;
+};
+
+export default { findBMI, formatOutput };
--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import BMICalculator from "./BMICalculator";
+
+const heightElement = document.getElementById("height");
+const ageElement = document.getElementById("age");
+const formElement = document.getElementById("form");
+const outputElement = document.getElementById("output");
+
+formElement.onsubmit = (event) => {
+  event.preventDefault();
+
+  const h = heightElement.value;
+  const a = ageElement.value;
+
+  const bmi = BMICalculator.findBMI(h, a);
+  const output = BMICalculator.formatOutput(bmi);
+
+  outputElement.innerHTML = output;
+};
